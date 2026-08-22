57499f075b1f22ac3eb90d99747ccfa17f4f2c6c
ck小捷 <49640015+44578287@users.noreply.github.com>
2026-08-22 14:26:20 +0800

    ui: inherit correct dark text color on mobile settings

--- ui/src/layout/components_setting/SettingsModalMobile.tsx
+++ ui/src/layout/components_setting/SettingsModalMobile.tsx
@@ -91,127 +91,108 @@ const SettingsModalMobile: React.FC<SettingsDialogProps> = () => {
 
   const renderContent = () => {
     switch (currentPage) {
-      case "general":
-        return <SettingsGeneral />;
-      case "network":
-        return <SettingsNetwork />;
-      case "access":
-        return <SettingsAccessIndex />;
-      case "hardware":
-        return <SettingsHardware />;
-      case "streaming":
-        return <SettingsStreaming />;
-      case "home_assistant":
-        return <SettingsHomeAssistant />;
-      case "advanced":
-        return <SettingsAdvanced />;
-      case "version":
-        return <SettingsVersion />;
-      default:
-        return null;
+      case "general": return <SettingsGeneral />;
+      case "network": return <SettingsNetwork />;
+      case "access": return <SettingsAccessIndex />;
+      case "hardware": return <SettingsHardware />;
+      case "streaming": return <SettingsStreaming />;
+      case "home_assistant": return <SettingsHomeAssistant />;
+      case "advanced": return <SettingsAdvanced />;
+      case "version": return <SettingsVersion />;
+      default: return null;
     }
   };
 
-  const renderMenuPage = () => {
-    return (
-
-      <div style={{ height: "100%", overflow: "hidden" }} className={"py-[10px]"}>
-        <Menu
-          mode="inline"
-          selectedKeys={[]}
-          className={`border-r-0 ${styles.customMenu}`}
-          onClick={handleMenuSelect}
-          style={{
-            border: "none",
-            height: "100%",
-            width: "100%",
-            padding: "0px 0",
-            backgroundColor: token.token.colorBgContainer,
-            overscrollBehavior: "none",
-          }}
-          items={menuItems.map(item => ({
-            key: item.key,
-            label: (
-              <div style={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                width: "100%",
-                padding: "0px 0",
-              }}>
-                <span style={{ fontSize: "16px" }}>{menuLabel(item)}</span>
-                <RightOutlined style={{
-                  fontSize: "12px",
-                  color: token.token.colorTextSecondary,
-                }} />
-              </div>
-            ),
-            icon: React.cloneElement(item.icon as React.ReactElement, {
-              // style: { fontSize: "18px" },
-            }),
-          }))}
-        />
-      </div>
-
-    );
-  };
-
-  const renderContentPage = () => {
-    return (
-      <Drawer
-        placement={"right"}
-        open={currentPage !== "menu"}
-        mask={false}
-        closable={false}
-        width={"100%"}
-        drawerRender={() => (
-          <div
-            className={cx(dark_bg2_style, "!pointer-events-auto")}
-            style={{
-              width: "100vw",
-              height: "100vh",
-              position: "fixed",
-              top: 0,
-              left: 0,
-              zIndex: 30000,
+  const renderMenuPage = () => (
+    <div style={{ height: "100%", overflow: "hidden" }} className="py-[10px] text-black dark:text-white">
+      <Menu
+        mode="inline"
+        selectedKeys={[]}
+        className={`border-r-0 ${styles.customMenu}`}
+        onClick={handleMenuSelect}
+        style={{
+          border: "none",
+          height: "100%",
+          width: "100%",
+          padding: "0px 0",
+          backgroundColor: token.token.colorBgContainer,
+          overscrollBehavior: "none",
+        }}
+        items={menuItems.map(item => ({
+          key: item.key,
+          label: (
+            <div style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "100%",
+              padding: "0px 0",
             }}>
-            <Layout style={{ height: "100%" }} className={dark_bg2_style}>
-              <Header
-                className={dark_bg2_style}
+              <span style={{ fontSize: "16px" }}>{menuLabel(item)}</span>
+              <RightOutlined style={{ fontSize: "12px", color: token.token.colorTextSecondary }} />
+            </div>
+          ),
+          icon: React.cloneElement(item.icon as React.ReactElement, {}),
+        }))}
+      />
+    </div>
+  );
+
+  const renderContentPage = () => (
+    <Drawer
+      placement="right"
+      open={currentPage !== "menu"}
+      mask={false}
+      closable={false}
+      width="100%"
+      drawerRender={() => (
+        <div
+          className={cx(dark_bg2_style, "!pointer-events-auto text-black dark:text-white")}
+          style={{
+            width: "100vw",
+            height: "100vh",
+            position: "fixed",
+            top: 0,
+            left: 0,
+            zIndex: 30000,
+          }}
+        >
+          <Layout style={{ height: "100%" }} className={`${dark_bg2_style} text-black dark:text-white`}>
+            <Header
+              className={dark_bg2_style}
+              style={{
+                backgroundColor: token.token.colorBgContainer,
+                borderBottom: `1px solid ${token.token.colorBorderSecondary}`,
+                padding: "0 16px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "56px",
+                lineHeight: "56px",
+                position: "relative",
+              }}
+            >
+              <Button
+                type="text"
+                icon={<ArrowLeftOutlined />}
+                onClick={handleBack2}
+                className={text_primary_color}
                 style={{
-                  backgroundColor: token.token.colorBgContainer,
-                  borderBottom: `1px solid ${token.token.colorBorderSecondary}`,
-                  padding: "0 16px",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  height: "56px",
-                  lineHeight: "56px",
-                  position: "relative",
-                }}>
-                <Button
-                  type="text"
-                  icon={<ArrowLeftOutlined />}
-                  onClick={handleBack2}
-                  className={text_primary_color}
-                  style={{
-                    marginRight: "16px",
-                    border: "none",
-                    boxShadow: "none",
-                    position: "absolute",
-                    left: "4px",
-                  }}
-                >{$at("Back")}</Button>
-                <span
-                  style={{
-                    fontSize: "16px",
-                    fontWeight: "400",
-                  }}
-                  className={text_color}>
-              {getPageTitle()}
-            </span>
-              </Header>
-              <Content style={{
+                  marginRight: "16px",
+                  border: "none",
+                  boxShadow: "none",
+                  position: "absolute",
+                  left: "4px",
+                }}
+              >
+                {$at("Back")}
+              </Button>
+              <span style={{ fontSize: "16px", fontWeight: "400" }} className={text_color}>
+                {getPageTitle()}
+              </span>
+            </Header>
+            <Content
+              style={{
                 padding: "16px",
                 overflow: "auto",
                 height: "calc(100% - 56px)",
@@ -219,24 +200,21 @@ const SettingsModalMobile: React.FC<SettingsDialogProps> = () => {
                 scrollbarWidth: "none",
                 msOverflowStyle: "none",
               }}
-              className="hide-scrollbar"
-              >
-                {renderContent()}
-              </Content>
-            </Layout>
-          </div>
-        )}
-        styles={{header:{height:"0px"}}}
-      >
-
-      </Drawer>
-    );
-  };
+              className="hide-scrollbar text-black dark:text-white"
+            >
+              {renderContent()}
+            </Content>
+          </Layout>
+        </div>
+      )}
+      styles={{ header: { height: "0px" } }}
+    />
+  );
 
   return (
     <>
       {renderMenuPage()}
-      {renderContentPage()}  
+      {renderContentPage()}
     </>
   );
 };
