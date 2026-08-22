221930b1c50c35fd65db19285c2ab292167a8311
ck小捷 <49640015+44578287@users.noreply.github.com>
2026-08-22 13:23:04 +0800

    ui: run browser WebRTC codec compatibility guard globally

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -11,6 +11,7 @@ import { ExclamationTriangleIcon } from "@heroicons/react/16/solid";
 
 import EmptyCard from "@components/EmptyCard";
 import NotFoundPage from "@components/NotFoundPage";
+import WebRTCCodecCompatibilityGuard from "@components/WebRTCCodecCompatibilityGuard";
 import { LocalDevice } from "@/layout/index.pc";
 import Card from "@components/Card";
 import LocalAuthPage, { DeviceStatus } from "@routes/login_page/index";
@@ -84,6 +85,7 @@ document.addEventListener("DOMContentLoaded", () => {
     <>
 
       <ThemeProvider>
+        <WebRTCCodecCompatibilityGuard />
         <RouterProvider router={router} />
         <Notifications
           toastOptions={{
